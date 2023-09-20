22626c48ee86b9cecd06395adce15c4bdcc65b90
Maria Gorodilova <mashunja13@yandex.ru>
2023-09-20 21:48:46 +0500

    исправления после ревью

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-import {useNavigate} from "react-router-dom";
 
 function NavBar({handleSignOut, userEmail}) {
-  const navigate = useNavigate();
-
-  function signOut() {
-    localStorage.removeItem('jwt');
-    handleSignOut();
-    navigate('/signin');
-  }
-
+  
   return (
     <div className="header__user">
       <p className="header__user-email">{userEmail}</p>
-      <a type="submit" onClick={signOut} className="header__link">Выйти</a>
+      <a type="submit" onClick={handleSignOut} className="header__link">Выйти</a>
     </div>
   )
 }
